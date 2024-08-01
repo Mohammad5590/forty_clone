c0a5aee9510a086df1e2e9b939d3f0c47c0d8ceb
Mohammad 5590 <faziluddin1978@gmail.com>
2024-08-01 18:54:04 +0530

    completed

---
+++ js/overlay.js
@@ -0,0 +1,12 @@
+const homeA=document.querySelector(".home")
+const landingA=document.querySelector(".landing")
+const genricA=document.querySelector(".genric")
+homeA.addEventListener("click", function(){
+    window.location.href="index.html"
+})
+landingA.addEventListener("click", function(){
+    window.location.href="landing.html"
+})
+genricA.addEventListener("click", function(){
+    window.location.href="genic.html"
+})
